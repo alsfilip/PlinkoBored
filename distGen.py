7a42fae51c691b3f6aab5345839467fbcbbc524f
Alex Filipowicz <alsfilip@gmail.com>
2015-08-19 14:37:02 -0400

    Adding code to generate distributions

---
+++ distGen.py
@@ -0,0 +1,9 @@
+'''
+August 19th, 2015
+
+This is a program to generate distributions for the PlinkoBored experiment. The distributions generated here will be added to the plinkoBored.py program and used during the experiment.
+'''
+
+import random
+
+ 
